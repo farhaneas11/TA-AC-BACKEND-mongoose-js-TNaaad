a047158e18ac91a859c8026e8274ba91819d1221
farhaneas11 <farhan.salam11@gmail.com>
2021-07-16 22:24:15 +0530

    mongoose-js-3

---
+++ block-BNaacj/sample/schema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var schema = mongoose.schema;
+
+var userschema = new Schema({
+    password : {type : String,minlength : 5 , maxlength : 15},
+    createdAt : { type :Date , default : Date.now}
+},{timestamps : true});
+
+
+var newschema = new schema({
+    email : {type : String , lowercase :  true},
+    age : {type :Number , default : 0}
+})
